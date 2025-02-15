e51f821a261011c30ab1e99b74f8793a9ec656ad
Maulana Adam Sahid <masadamsahid@gmail.com>
2025-02-15 13:42:37 +0700

    Landing page's category list

--- src/app/_components/CategorySearch.tsx
+++ src/app/_components/CategorySearch.tsx
@@ -1,22 +1,59 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
+import GlobalApi from "@/app/_utils/GlobalApi";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+type Category = {
+  id: number;
+  documentId: string;
+  Name: string;
+  Icon: { url: string };
+};
 
 type Props = {}
 
 const CategorySearch = (props: Props) => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    getCategoryList();
+  }, []);
+
+  const getCategoryList = () => {
+    GlobalApi.getCategory().then(res => {
+      console.log(res);
+      setCategories(res.data.data);
+    });
+  }
+
   return (
     <div className="mb-10 flex flex-col items-center gap-4">
-      <h2 className="font-bold text-4xl tracking-wide">
+      <h2 className="font-bold text-4xl tracking-wide text-center">
         Search <span className="text-primary">Doctors</span>
       </h2>
-      <p className="text-gray-500 text-xl">Seach doctor and book appointment in one click</p>
+      <p className="text-gray-500 text-xl text-center mx-2">Seach doctor and book appointment in one click</p>
 
-      <div className="mt-3 flex w-full max-w-sm items-center space-x-2">
+      <div className="mt-3 flex flex-col sm:flex-row w-full max-w-sm items-center gap-2 p-4">
         <Input type="text" placeholder="Search..." />
-        <Button type="submit">
+        <Button type="submit" className="w-full sm:w-fit">
           <Search className="size-4 mr-2" /> Search
         </Button>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-2 sm:gap-4 mt-5">
+        {categories.map((c, i) => {
+          if(i+1 > 6) return null;
+
+          return (
+            <div key={c.documentId} className="flex flex-col text-center gap-2 items-center p-5 bg-primary/10 rounded-lg hover:scale-110 transition-all ease-in-out cursor-pointer">
+              <Image src={c.Icon.url} alt={c.Name} width={40} height={40} />
+              <label className="text-primary text-sm">{c.Name}</label>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ src/app/_utils/GlobalApi.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+
+const API_KEY = process.env.NEXT_PUBLIC_DOKTERIN_STRAPI_API_TOKEN!;
+
+const axiosClient = axios.create({
+  baseURL: 'http://localhost:1337/api/',
+  headers: {
+    'Authorization': `Bearer ${API_KEY}`,
+  },
+});
+
+const getCategory = () => axiosClient.get('/categories?populate=*');
+
+export default {
+  getCategory
+};
